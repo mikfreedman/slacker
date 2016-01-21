4c4c7f5151741bd47448cd9cd85fe270da072b88
Mik Freedman <pair+mfreedman@pivotallabs.com>
2016-01-21 11:30:41 +1100

    WIP

--- test/slacker/bot_test.exs
+++ test/slacker/bot_test.exs
@@ -31,26 +31,6 @@ defmodule Slacker.BotTest do
     put_in(echo_message[:text], "not a command")
   end
 
-  def channel_join_message do
-    %{
-      type: "message",
-      subtype: "channel_join",
-      text: "<@U2147483828|cal> has joined the channel",
-      channel: "the_channel",
-      user: "U2147483828",
-    }
-  end
-  
-  def channel_leave_message do
-    %{
-      type: "message",
-      subtype: "channel_leave",
-      text: "<@U2147483828|cal> has left the channel",
-      channel: "the_channel",
-      user: "U2147483828",
-    }
-  end
-
   def slack_details do
     %{
       me: %{
@@ -64,16 +44,6 @@ defmodule Slacker.BotTest do
       event_manager: new_event_manager,
       command_prefixes: ["slacker"],
     }
-  end
-
-  test "#handle_message dispatches an channel_join event to the event manager" do
-    Slacker.Bot.handle_message(channel_join_message, slack_details, state)
-    assert_receive %{gen_event_message: {{:event, "channel_join", "U2147483828"}, _meta}}, 100
-  end
-  
-  test "#handle_message dispatches an channel_leave event to the event manager" do
-      Slacker.Bot.handle_message(channel_leave_message, slack_details, state)
-      assert_receive %{gen_event_message: {{:event, "channel_leave", "U2147483828"}, _meta}}, 100
   end
 
   test "#handle_message dispatches an echo command to the event manager" do
